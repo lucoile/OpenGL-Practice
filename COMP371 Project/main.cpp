ba80ddf8d649d299f296e9c1f7e4eeb1a0f5e035
Thomas Buffard <buffardt@gmail.com>
2020-06-26 15:55:28 -0500

    Initial Commit

---
+++ COMP371 Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  COMP371 Project
+//
+//  Created by Thomas Buffard on 6/26/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
